bca8c627d933045217083f9ae465d1afba7b782a
TechnoPustak <73098888+TechnoPustak@users.noreply.github.com>
2020-10-25 09:34:48 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <title>Ashutosh Thakur</title>
+</head>
+<body>
+  <?php
+  $num = 405 + 15;
+  echo "Ashutosh is\n";
+  echo "$num";
+  ?>
+  <br><img src="index.jpg">
+</body>
+</html>
